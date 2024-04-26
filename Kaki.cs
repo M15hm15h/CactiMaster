192d5d2fe1e3fe106019c60ac8f8718d12449146
Mi15mi15 <michaelmargulyan@gmail.com>
2024-04-26 17:55:24 +0300

    Hi Cacti

---
+++ Kaki.cs
@@ -0,0 +1,11 @@
+class Kaki
+{
+    private string name;
+    private bool IsKaki;
+    public Kaki(string name, bool IsKaki)
+    {
+        this.name = name;
+        this.IsKaki = IsKaki;
+    }
+    public string GetName() { return this.name; }
+}
